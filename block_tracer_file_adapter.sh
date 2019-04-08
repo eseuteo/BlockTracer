9048fa7456e6cee4006c45d42087ccb1ba7c3beb
eseuteo <ricardoholthausen@gmail.com>
2019-04-08 18:40:12 +0200

    Now output filename is needed for the adapter

--- block_tracer_file_adapter.sh
+++ block_tracer_file_adapter.sh
@@ -5,7 +5,7 @@ tail -n +17 $1 > ./frags_file.csv
 python3 block_tracer_file_adapter.py ./header_file.txt ./frags_file.csv
 rm header_file.txt
 rm frags_file.csv
-cat length_file.txt output_df.csv > "bt_"$1
-echo "0,0,0,0,0,Null event" >> "bt_"$1
+cat length_file.txt output_df.csv > $2
+echo "0,0,0,0,0,Null event" >> $2
 rm length_file.txt
 rm output_df.csv
